9f8d0f446e4d12b39dcb77b8532944f62e510a9b
Michael Tandoc <michael.tandoc@ryerson.ca>
2022-11-19 13:48:07 -0500

    Added address Line 1 & Line 2 fields to checkout

--- src/Template/Checkout.php
+++ src/Template/Checkout.php
@@ -85,10 +85,18 @@ class Checkout extends Layout
                                 </div>
                             </div>
                             <div class="form-field form-field--required">
-                                <label class="form-field__label" for="billing_address_line1">Street Address: <span class="form-field__label-required"> (required)</span></label>
+                                <label class="form-field__label" for="billing_address_line1">Street Address (Line 1): <span class="form-field__label-required"> (required)</span></label>
                                 <div class="form-field__contents">
                                     <div class="textarea__container">
-                                        <textarea name="billing_address_line1" id="billing_address_line1" class="textarea" rows="2" cols="35"></textarea>
+                                        <input type="text" name="billing_address_line1" id="billing_address_line1" class="entry" size="35" maxlength="255" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-field">
+                                <label class="form-field__label" for="billing_address_line2">Street Address (Line 2): </label>
+                                <div class="form-field__contents">
+                                    <div class="textarea__container">
+                                        <input type="text" name="billing_address_line2" id="billing_address_line2" class="entry" size="35" maxlength="255" />
                                     </div>
                                 </div>
                             </div>
